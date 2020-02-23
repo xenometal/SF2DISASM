15ddca4cec0f9e3a27d76649806b6685ac35f7ce
xenometal <2898219+xenometal@users.noreply.github.com>
2020-02-22 20:07:16 -0500

    Added handling for a greater number of centaur battle sprites

---
+++ disasm/code/gameflow/battle/battlescenes/getallyanimation-sf2gu.asm
@@ -0,0 +1,80 @@
+
+; ASM FILE code\gameflow\battle\battlescenes\getallyanimation-sf2gu.asm :
+; GetAllyAnimation routine (alternate implementation)
+
+; =============== S U B R O U T I N E =======================================
+
+; Alternate implementation of the ally battle sprite index getter routine.
+;   Supports the association of special spear throwing animations to
+;   a greater number of knight-type battle sprites.
+;
+;   Version 1.0 - February 18th, 2020
+;   by Xenometal
+;
+;   In: D1 = animation type, Out: A0 = animation pointer
+
+GetAllyAnimation:
+        
+@CENTAUR_BATTLESPRITES_COUNTER: equ @SPEARTHROW_ANIMATIONS_OFFSET-1
+@SPEARTHROW_ANIMATIONS_OFFSET:  equ tbl_SpearThrowAnimations-tbl_CentaurBattleSprites
+@ANIMATIONTYPE_DODGE:           equ 1
+        
+        move.l  d1,-(sp)
+        move.w  d7,-(sp)
+        
+        ; Check if regular attack animation type
+        tst.w   d1
+        bne.s   @CheckSpecialAnimationType
+        
+; ---------------------------------------------------------------------------
+        
+        ; Check if weapon sprite is a spear
+        cmpi.w  #SPEAR_WEAPONSPRITES_START,((CHARACTER_WEAPON_SPRITE-$1000000)).w
+        blo.s   @GetAnimationIndex
+        cmpi.w  #SPEAR_WEAPONSPRITES_END,((CHARACTER_WEAPON_SPRITE-$1000000)).w
+        bhi.s   @GetAnimationIndex
+        
+        ; Check if battle sprite is a centaur
+        lea     tbl_CentaurBattleSprites(pc), a0
+        move.w  ((BATTLESCENE_ACTOR-$1000000)).w,d1
+        move.w  #@CENTAUR_BATTLESPRITES_COUNTER,d7
+        
+@Loop:
+        cmp.b   (a0,d7.w),d1
+        beq.s   @GetSpearThrowAnimation
+        dbf     d7,@Loop
+        bra.s   @GetAnimationPointer
+        
+@GetSpearThrowAnimation:
+        clr.w   d1
+        move.b  @SPEARTHROW_ANIMATIONS_OFFSET(a0,d7.w),d1
+        bra.s   @GetAnimationPointer
+        
+; ---------------------------------------------------------------------------
+        
+@CheckSpecialAnimationType:
+        cmpi.w  #SPECIAL_ANIMATIONS_START,d1
+        bhs.s   @GetAnimationPointer
+        
+        ; Check if dodge animation type
+        cmpi.w  #@ANIMATIONTYPE_DODGE,d1
+        bne.s   @Default
+        moveq   #DODGE_ANIMATIONS_START,d1
+        bra.s   @GetAnimationIndex
+        
+@Default:
+        clr.w   d1                                      ; default to regular attack animation
+        
+@GetAnimationIndex:
+        add.w   ((BATTLESCENE_ACTOR-$1000000)).w,d1
+        
+@GetAnimationPointer:
+        movea.l (p_pt_AllyAnimations).l,a0
+        lsl.w   #2,d1
+        movea.l (a0,d1.w),a0
+        
+        move.w  (sp)+,d7
+        move.l  (sp)+,d1
+        rts
+        
+    ; End of function GetAllyAnimation
--- disasm/layout/sf2-04-0x018000-0x020000.asm
+++ disasm/layout/sf2-04-0x018000-0x020000.asm
@@ -4,11 +4,14 @@
 ; FREE SPACE : 532 bytes.
 
 
-                include "code\gameflow\battle\battlescenes\battlesceneengine_0.asm"    ; Battlescene engine
-                
                 if (PROJECT_SF2GU=1)
+                include "code\gameflow\battle\battlescenes\battlesceneengine_0-sf2gu_0.asm"    ; Battlescene engine
+                include "code\gameflow\battle\battlescenes\getallyanimation-sf2gu.asm"    ; GetAllyAnimation routine (alternate implementation)
+                align $19838
+                include "code\gameflow\battle\battlescenes\battlesceneengine_0-sf2gu_1.asm"    ; Battlescene engine
                 include "data\stats\allies\allybattlesprites-sf2gu.asm"
                 else
+                include "code\gameflow\battle\battlescenes\battlesceneengine_0.asm"    ; Battlescene engine
                 include "data\stats\allies\allybattlesprites.asm"    ; Ally battle sprites table
                 endif
                 
@@ -25,4 +28,9 @@
                 
                 include "data\graphics\battles\battlesprites\enemyidlebattlesprites.asm"    ; Enemy Idle Battle Sprites
                 include "code\gameflow\battle\battlescenes\battlesceneengine_2.asm"    ; Battlescene engine
+                
+                if (PROJECT_SF2GU=1)
+                include "data\stats\allies\centaurbattlesprites-sf2gu.asm" ; Centaur battle sprite and spear throw animation indexes table
+                endif
+                
                 align $20000
