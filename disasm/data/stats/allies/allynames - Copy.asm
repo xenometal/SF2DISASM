81bd51c8459b3fd50d67ffe252e569edd96fafcc
Erikin84 <erik.fiedler@hotmail.com>
2020-03-01 01:12:25 -0500

    Small changes to map sprites/icon color for unused slots

---
+++ disasm/data/stats/allies/allynames - Copy.asm
@@ -0,0 +1,40 @@
+
+; ASM FILE data\stats\allies\allynames.asm :
+; 0xFAD6..0xFB8A : Ally names
+AllyNames:      allyName "BOWIE"
+                allyName "SARAH"
+                allyName "CHESTER"
+                allyName "JAHA"
+                allyName "KAZIN"
+                allyName "SLADE"
+                allyName "KIWI"
+                allyName "PETER"
+                allyName "MAY"
+                allyName "GERHALT"
+                allyName "LUKE"
+                allyName "ROHDE"
+                allyName "RICK"
+                allyName "ELRIC"
+                allyName "ERIC"
+                allyName "KARNA"
+                allyName "RANDOLF"
+                allyName "TYRIN"
+                allyName "JANET"
+                allyName "HIGINS"
+                allyName "SKREECH"
+                allyName "TAYA"
+                allyName "FRAYJA"
+                allyName "JARO"
+                allyName "GYAN"
+                allyName "SHEELA"
+                allyName "ZYNK"
+                allyName "CHAZ"
+                allyName "LEMON"
+                allyName "CLAUDE"
+                
+                ; Additional entries to be defined with force members expansion patch
+                if (FORCE_MEMBERS_EXPANSION=1)
+                allyName ""
+                allyName ""
+                endif
+                
